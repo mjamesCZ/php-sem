68da30c33b61725f7f80c9d3552bc8697a8223d7
Jakub Šlambor <63538910+mjamesCZ@users.noreply.github.com>
2022-06-10 09:39:22 +0200

    Admin fixup

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,7 +16,7 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-        if (auth()->user()->admin) {
+        if (auth()->user() && auth()->user()->admin) {
             return $next($request);
         }
 
